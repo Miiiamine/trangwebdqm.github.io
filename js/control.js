6f5c80feed4e911e6fc46423fefa5ccbe9ca17f4
Miiiamine <140496907+Miiiamine@users.noreply.github.com>
2023-08-11 05:08:26 +0700

    add_fileweb_dqm

---
+++ js/control.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+$('#sp-men').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-men1').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
